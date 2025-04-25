4c8cb30570aae5fc58026681dd3c0b2c527c458a
DoktorNiczego <neron1051@gmail.com>
2025-04-25 13:13:29 +0200

    minor correction of comments

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
-
-
 class PasswordEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     service = db.Column(db.String(80), nullable=False)
@@ -38,9 +36,11 @@ class PasswordEntry(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref=db.backref('passwords', lazy=True))
 
+#pseudo-randomizer
 def pass_gen(size, chars=string.ascii_letters + string.digits + string.punctuation):
         return ''.join(random.choice(chars) for _ in range(size))
 
+#key generating function
 def load_create_key():
     if os.path.exists("key.key"):
         with open("key.key", "rb") as key_file:
@@ -88,27 +88,27 @@ def register():
 @app.route('/login', methods = ['POST'])
 def login():
     data = request.get_json()
-
+#checking if data is valid
     if not data or 'username' not in data or 'password' not in data:
          return jsonify({"error": "Provide requested information"}), 400 #sending data as json
     
     user_name = data['username']
     raw_password = data['password']
-
+#checking data is in db
     user = User.query.filter_by(username=user_name).first()
     if not user or not check_password_hash(user.password_hash, raw_password):
         return jsonify({"error": "Provided username or password is not correct"}), 400
-
+#creating token for logged user
     token = jwt.encode({'public_id': user.id, 
                         'exp': datetime.datetime.now() + datetime.timedelta(minutes=15)}, 
                         app.config['SECRET_KEY'], 
                         algorithm="HS256")
     return jsonify({"message": "Login successful", "token": token}), 200
-#work in progress----------------------------------
+#jwt verification decorator
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        
+#checking header with jwt        
         auth_request = request.headers.get("Authorization")
         if auth_request:
             try:
@@ -117,13 +117,13 @@ def token_required(f):
                 return jsonify({"error": "Invalid token format"})
         else:
             return jsonify({"error": "Missing token"}), 401
-#finish it!!!        
+#decoding token if not expierd     
         try:
             decoded = jwt.decode(token, 
                                  app.config['SECRET_KEY'], 
                                  options={"verify_exp": True}, 
                                  algorithm="HS256")
-            
+#checking user id            
             user = User.query.get(decoded['public_id'])
             if not user:
                 return jsonify({"error": "User not found"}), 404
@@ -133,10 +133,11 @@ def token_required(f):
 
         return f(user, *args, **kwargs)
     return decorated
-
+#password generating function
 @app.route('/generate-password', methods = ['POST'])
-@token_required
+@token_required #verification decorator
 def generate_password(user):
+#getting data from user
     data = request.get_json()
     current_time = datetime.datetime.now()
     if not data or 'length' not in data or 'service' not in data:
@@ -147,13 +148,13 @@ def generate_password(user):
     
     if not isinstance(length, int) or length < 5:
          return jsonify({"error": "Length must be a number bigger than 4"}), 400
-
-    password = pass_gen(length)#passing length to randomizer
-
-    key = load_create_key()#creating or using existing key
+#generating random password
+    password = pass_gen(length)
+#creating key and encryption
+    key = load_create_key()
     cipher = Fernet(key)
     encrypted_password = cipher.encrypt(password.encode())#encrypting password
-
+#pushing input and password to db
     new_record = PasswordEntry(service=input_service, 
                                plain_password=encrypted_password, 
                                timestamp=current_time,
